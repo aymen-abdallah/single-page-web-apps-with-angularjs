bfdb336d73e0acb09843f1f0a724ef840414b31e
Aymen Abdallah <aymen.abdallah@gmail.com>
2016-10-14 11:04:26 +0200

    first commit

---
+++ assignments/assignment1/app.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+angular.module('Assignment1App', [])
+.controller('Assignment1Controller', DIController);
+
+function Assignment1Controller ($scope,
+                       $filter,
+                       $injector) {
+  $scope.name = "";
+
+})();
