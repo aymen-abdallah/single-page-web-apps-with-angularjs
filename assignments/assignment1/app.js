9b3464f44c25fd25b1b7e3d3b01f6647cfcfa1e8
Aymen Abdallah <aymen.abdallah@gmail.com>
2016-10-14 17:23:13 +0200

    Items check is done!

--- assignments/assignment1/app.js
+++ assignments/assignment1/app.js
@@ -1,12 +1,44 @@
 (function () {
 'use strict';
 
-angular.module('Assignment1App', [])
-.controller('Assignment1Controller', DIController);
+angular.module('assignment1App', [])
+.controller('Assignment1Controller', Assignment1Controller);
 
-function Assignment1Controller ($scope,
-                       $filter,
-                       $injector) {
-  $scope.name = "";
+Assignment1Controller.$inject = ['$scope'];
+
+function Assignment1Controller ($scope) {
+  $scope.lunchMenuItems = "";
+  $scope.message = "";
+
+  $scope.doCheckMenu = function(){
+	  if($scope.lunchMenuItems == null || $scope.lunchMenuItems === ""){
+        $scope.message = "Please enter data first";
+        return;
+    }
+    var list = $scope.lunchMenuItems.split(","),
+        size = 0;
+    console.log("The elements are : " + list);
+
+    for(var i = 0; i < list.length; i++){
+        if(list[i].trim() !== ""){
+          size++;
+        }
+    }
+
+    console.log("The size of the elements is : " + size);
+    if(size === 0){
+      $scope.message = "Empty items!";
+      return;
+    }
+
+    if(size <= 3){
+      $scope.message = "Enjoy!";
+    } else {
+      $scope.message = "Too much!";
+    }
+
+  };
+
+}
 
 })();
