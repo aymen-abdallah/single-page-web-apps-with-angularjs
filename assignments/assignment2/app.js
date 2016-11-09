389d098192a067ef4e1a36f9ac0e3db2a3035e7d
Aymen Abdallah <aymen.abdallah@gmail.com>
2016-11-09 10:26:30 +0100

    add provider to shopping list module

---
+++ assignments/assignment2/app.js
@@ -0,0 +1,84 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.provider('ShoppingListCheckOffService', ShoppingListCheckOffServiceProvider)
+.config(Config);
+
+Config.$inject = ['ShoppingListCheckOffServiceProvider'];
+function Config(ShoppingListCheckOffServiceProvider) {
+  ShoppingListCheckOffServiceProvider.defaults.maxItems = 5;
+}
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+
+  toBuy.everythingBought = false;
+  toBuy.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
+
+  toBuy.buyItem = function (itemIdex) {
+    try {
+      ShoppingListCheckOffService.buyItem(itemIdex);
+    } catch (error) {
+      toBuy.everythingBought = true;
+    }
+  }
+}
+
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var alreadyBought = this;
+
+  alreadyBought.emptyList = false;
+  alreadyBought.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService(maxItems) {
+  var service = this;
+
+  // List of shopping items
+  var toBuyItems = [{ name: "cookies", quantity: 10 }, { name: "breads", quantity: 7 },
+  { name: "eggs", quantity: 12 }, { name: "apple", quantity: 3 }, { name: "cheese", quantity: 3 }],
+  boughtItems = [];
+
+  service.buyItem = function (itemIdex) {
+    if ((maxItems === undefined) ||
+        (maxItems !== undefined) && (boughtItems.length < maxItems)) {
+      var item = toBuyItems[itemIdex];
+      toBuyItems.splice(itemIdex, 1);
+      boughtItems.push(item);
+    }
+    if((maxItems !== undefined) && (boughtItems.length === maxItems)) {
+      throw new Error("Max items (" + maxItems + ") reached.");
+    }
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+function ShoppingListCheckOffServiceProvider() {
+  var provider = this;
+
+  provider.defaults = {
+    maxItems: 2
+  };
+
+  provider.$get = function () {
+    var shoppingListCheckOff = new ShoppingListCheckOffService(provider.defaults.maxItems);
+
+    return shoppingListCheckOff;
+  };
+}
+
+})();
